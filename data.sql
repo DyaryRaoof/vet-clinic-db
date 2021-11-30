22dd3c2b3e92a38aef82920ee56b48871fac5a0b
Dyary Raoof Bayz <dyary2020@gmail.com>
2021-12-01 00:45:12 +0300

    Delete data and use transactions

--- data.sql
+++ data.sql
@@ -10,8 +10,30 @@ INSERT INTO animals (name, date_of_birth, ecape_attempts, neutered, weight_kg)VA
 
 
 
-INSERT INTO animals (name, date_of_birth, ecape_attempts, neutered, weight_kg)VALUES ('Charmander', To_DATE('2,8,2020', 'MM, DD, YY'), 0 ,true,11);
-INSERT INTO animals (name, date_of_birth, ecape_attempts, neutered, weight_kg)VALUES ('Plantmon', To_DATE('11,15,2022', 'MM, DD, YY'), 2 ,true,5.7);
-INSERT INTO animals (name, date_of_birth, ecape_attempts, neutered, weight_kg)VALUES ('Squirtle', To_DATE('4,2,1993', 'MM, DD, YY'), 3 ,false,12.13);
-INSERT INTO animals (name, date_of_birth, ecape_attempts, neutered, weight_kg)VALUES ('Angemon', To_DATE('6,12,2005', 'MM, DD, YY'), 1 ,true,45);
+INSERT INTO animals (name, date_of_birth, ecape_attempts, neutered, weight_kg)VALUES ('Charmander', To_DATE('2,8,2020', 'MM, DD, YY'), 0 ,true,-11);
+INSERT INTO animals (name, date_of_birth, ecape_attempts, neutered, weight_kg)VALUES ('Plantmon', To_DATE('11,15,2022', 'MM, DD, YY'), 2 ,true,-5.7);
+INSERT INTO animals (name, date_of_birth, ecape_attempts, neutered, weight_kg)VALUES ('Squirtle', To_DATE('4,2,1993', 'MM, DD, YY'), 3 ,false,-12.13);
+INSERT INTO animals (name, date_of_birth, ecape_attempts, neutered, weight_kg)VALUES ('Angemon', To_DATE('6,12,2005', 'MM, DD, YY'), 1 ,true,-45);
 INSERT INTO animals (name, date_of_birth, ecape_attempts, neutered, weight_kg)VALUES ('Blossom', To_DATE('10,13,1998', 'MM, DD, YY'), 3,true,17);
+
+BEGIN;
+UPDATE animals SET species = 'unspecified';
+ROLLBACK;
+
+BEGIN;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+COMMIT;
+
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
+
+BEGIN;
+
+DELETE FROM animals WHERE date_of_birth > '1,1,2022';
+SAVEPOINT point1;
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK TO SAVEPOINT point1;
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+COMMIT;
