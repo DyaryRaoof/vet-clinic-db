59acade18a18683d18ba40c8bedf39ed08a4c345
Dyary Raoof Bayz <dyary2020@gmail.com>
2021-12-01 21:59:48 +0300

    Alter schema

--- schema.sql
+++ schema.sql
@@ -11,3 +11,17 @@ CREATE TABLE animals (
 );
 
 ALTER TABLE animals ADD species varchar(255);
+
+
+CREATE TABLE owners (id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY, full_name VARCHAR(255), age INT);
+CREATE TABLE species (id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY, name VARCHAR(255));
+
+ALTER TABLE animals ADD PRIMARY KEY (id);
+
+ALTER TABLE animals DROP COLUMN  species;
+
+ALTER TABLE animals ADD species_id INT;
+ALTER TABLE animals ADD  FOREIGN KEY(species_id) REFERENCES species(id);
+ALTER TABLE animals ADD owner_id INT;
+ALTER TABLE animals ADD  FOREIGN KEY(owner_id) REFERENCES owners(id);
+
